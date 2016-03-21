b58f41d833b64a717b6ee21b802339e38436f0d9
Paul Yarabe <paulyarabe@post.harvard.edu>
2016-03-21 09:55:59 +0000

    debugging production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Nomster::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'nomster-py.heroku.com',
+    :domain         => 'yourapp.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
